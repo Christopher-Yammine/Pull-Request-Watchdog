9ff96849e95bd92dfa1be20749c11062c02c7973
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-14 16:40:33 +0300

    creating a function to output PRs with review requests

--- Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
+++ Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
@@ -48,4 +48,44 @@ class WatchdogController extends Controller
             }
         }
     }
+    public function getRRPullRequests()
+    {
+        $n = 2;
+       
+
+        for ($i = 1; $i < $n; $i++) {
+
+            $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?&per_page=100&page=" . $i;
+
+
+            $headers = [
+                "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine"
+            ];
+            $curl = curl_init($url);
+
+
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+            //for debug only!
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            $resp = curl_exec($curl);
+
+            curl_close($curl);
+            $pull_requests = "";
+            $pull_requests = json_decode($resp, false);
+
+
+            for ($j = 0; $j < count($pull_requests); $j++) {
+                
+                    echo $pull_requests[$j]->created_at . "\n";
+                
+            }
+            if (count($pull_requests) === 100) {
+                ++$n;
+            }
+        }
+    }
 }
