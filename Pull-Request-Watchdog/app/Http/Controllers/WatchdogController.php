72a85d64f760a2485cd14b74adc55b1ef2ec5a25
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-14 07:34:11 +0300

    creating a for loop to get all the the requests due to limitations

--- Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
+++ Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
@@ -8,33 +8,38 @@ class WatchdogController extends Controller
 {
     public function getPullRequests()
     {
-        $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100";
-        $headers = [
-            "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine"
-        ];
-        $curl = curl_init($url);
+        for ($i = 1; $i <= 3; $i++) {
+
+            $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100&page=" . $i;
+            
+
+            $headers = [
+                "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine"
+            ];
+            $curl = curl_init($url);
 
 
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        //for debug only!
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        $resp = curl_exec($curl);
+            //for debug only!
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            $resp = curl_exec($curl);
+            
+            curl_close($curl);
+            $pull_requests = "";
+            $pull_requests = json_decode($resp, false);
 
-        curl_close($curl);
 
-        $pull_requests = json_decode($resp, false);
+            for ($j = 0; $j < count($pull_requests); $j++) {
+                echo $pull_requests[$j]->url . "\n";
+            }
+            print_r(count($pull_requests));
 
-
-        for($i=0;$i<count($pull_requests);$i++){
-            print_r($pull_requests[$i]->url . "\n");
+            // $mydate = date("Y-m-d", strtotime("-2 week"));
+            // print_r($mydate);
         }
-        print_r(count($pull_requests));
-
-        $mydate = date("Y-m-d", strtotime("-2 week"));
-        print_r($mydate);
     }
 }
