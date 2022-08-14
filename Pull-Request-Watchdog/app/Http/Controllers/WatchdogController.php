a9b6d53513895f2815c8cddf3a65c2a2fe0cbd4b
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-14 23:04:17 +0300

    refactoring the code and renaming variables

--- Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
+++ Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-ini_set('max_execution_time', '300');
 
 class WatchdogController extends Controller
 {
@@ -19,6 +19,7 @@ class WatchdogController extends Controller
             "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine",
             "authorization: Bearer " . env("TOKEN")
         ];
+        $fileLink = "";
         $filename = "./Downloads/1-old-pull-requests.csv";
         $output = "";
         for ($i = 1; $i < $n; $i++) {
@@ -49,15 +50,20 @@ class WatchdogController extends Controller
                 ++$n;
             }
         }
-        file_put_contents($filename, $output);
 
-        echo "Done registering old PRs";
+        file_put_contents($filename, $output);
+        $rawLink = explode("./", $filename)[1];
+        $fileLink .= env("DOWNLOADS_URL") . $rawLink;
+        return response()->json([
+            "status" => "success",
+            "link" => $fileLink
+        ], 200);
     }
 
     public function getRRPullRequests()
     {
         $n = 2;
-
+        $fileLink = "";
         $headers = [
             "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine",
             "authorization: Bearer " . env("TOKEN")
@@ -89,27 +95,30 @@ class WatchdogController extends Controller
             }
         }
         file_put_contents($filename, $output);
-        echo "Done registering review required PRs";
+        $rawLink = explode("./", $filename)[1];
+        $fileLink .= env("DOWNLOADS_URL") . $rawLink;
+        return response()->json([
+            "status" => "success",
+            "link" => $fileLink
+        ], 200);
     }
     public function getSuccessPullRequests()
     {
+        ini_set('max_execution_time', '300');
         $headers = [
             "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine",
             "authorization: Bearer " . env("TOKEN")
         ];
         $n = 2;
         $filename = "./Downloads/3-Successful-PRs.csv";
+        $fileLink = "";
         $output = "";
 
         for ($i = 1; $i < $n; $i++) {
 
             $url = env("BASE_URL") . $i;
-
-
-
+           
             $curl = curl_init($url);
-
-
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
@@ -120,27 +129,16 @@ class WatchdogController extends Controller
             curl_close($curl);
             $pull_requests = "";
             $pull_requests = json_decode($resp, false);
-
-
             for ($j = 0; $j < count($pull_requests); $j++) {
-
-
                 $url2 = "https://api.github.com/repos/woocommerce/woocommerce/commits/" . $pull_requests[$j]->head->sha . "/status";
-
-
-
                 $curl2 = curl_init($url2);
-
-
                 curl_setopt($curl2, CURLOPT_URL, $url2);
                 curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
-
-                //for debug only!
                 curl_setopt($curl2, CURLOPT_SSL_VERIFYHOST, false);
                 curl_setopt($curl2, CURLOPT_SSL_VERIFYPEER, false);
                 curl_setopt($curl2, CURLOPT_HTTPHEADER, $headers);
                 $resp2 = curl_exec($curl2);
-
+              
                 curl_close($curl2);
                 $pull_requests2 = "";
                 $pull_requests2 = json_decode($resp2, false);
@@ -154,11 +152,17 @@ class WatchdogController extends Controller
             }
         }
         file_put_contents($filename, $output);
-        echo "Done registering PRs with success status";
+        $rawLink = explode("./", $filename)[1];
+        $fileLink .= env("DOWNLOADS_URL") . $rawLink;
+        return response()->json([
+            "status" => "success",
+            "link" => $fileLink
+        ], 200);
     }
     public function getUnassignedPullRequests()
     {
         $filename = "./Downloads/4-Unassigned-PRs.csv";
+        $fileLink = "";
         $output = "";
         $headers = [
             "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine",
@@ -192,6 +196,11 @@ class WatchdogController extends Controller
             }
         }
         file_put_contents($filename, $output);
-        echo "Done registering unassigned PRs";
+        $rawLink = explode("./", $filename)[1];
+        $fileLink .= env("DOWNLOADS_URL") . $rawLink;
+        return response()->json([
+            "status" => "success",
+            "link" => $fileLink
+        ], 200);
     }
 }
