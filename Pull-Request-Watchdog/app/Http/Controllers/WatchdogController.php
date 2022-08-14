d10167234d5d5d0074428736897035db6a562721
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-14 07:05:23 +0300

    modifiying the number of requests

--- Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
+++ Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
@@ -8,8 +8,10 @@ class WatchdogController extends Controller
 {
     public function getPullRequests()
     {
-        $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls";
-        $headers = ["Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine"];
+        $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100";
+        $headers = [
+            "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine"
+        ];
         $curl = curl_init($url);
 
 
@@ -21,19 +23,18 @@ class WatchdogController extends Controller
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         $resp = curl_exec($curl);
-        
+
         curl_close($curl);
 
         $pull_requests = json_decode($resp, false);
-      
-        // $chi=array_column($beers,"id");
-        // $element=array_rand($chi,1);
-        // for($i=0;$i<count($pull_requests);$i++){
-        //     print_r($pull_requests[$i]->url . "\n");
-        // }
-        
-        $mydate=date("Y-m-d", strtotime("-2 week"));
-        print_r($mydate); 
-        
+
+
+        for($i=0;$i<count($pull_requests);$i++){
+            print_r($pull_requests[$i]->url . "\n");
+        }
+        print_r(count($pull_requests));
+
+        $mydate = date("Y-m-d", strtotime("-2 week"));
+        print_r($mydate);
     }
 }
