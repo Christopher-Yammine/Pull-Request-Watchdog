2ea738a6a97e880a27591f61e33ad46c5b844547
ChristopherYammine <chrisyammine98@gmail.com>
2022-08-14 16:33:39 +0300

    outputting the PRs created more than 14 days ago

--- Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
+++ Pull-Request-Watchdog/app/Http/Controllers/WatchdogController.php
@@ -6,12 +6,17 @@ use Illuminate\Http\Request;
 
 class WatchdogController extends Controller
 {
-    public function getPullRequests()
+    public function get14DaysPullRequests()
     {
-        for ($i = 1; $i <= 3; $i++) {
+        $n = 2;
+        $mydate = date("Y-m-d", strtotime("-2 week"));
+        $mytime = date("H:i:s");
+        $formattedDate = $mydate . "T" . $mytime . "Z";
 
-            $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?per_page=100&page=" . $i;
-            
+        for ($i = 1; $i < $n; $i++) {
+
+            $url = "https://api.github.com/repos/woocommerce/woocommerce/pulls?&per_page=100&page=" . $i;
+
 
             $headers = [
                 "Accept:application/vnd.github+json", "User-Agent: Christopher-Yammine"
@@ -27,19 +32,20 @@ class WatchdogController extends Controller
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
             $resp = curl_exec($curl);
-            
+
             curl_close($curl);
             $pull_requests = "";
             $pull_requests = json_decode($resp, false);
 
 
             for ($j = 0; $j < count($pull_requests); $j++) {
-                echo $pull_requests[$j]->url . "\n";
+                if ($pull_requests[$j]->created_at < $formattedDate) {
+                    echo $pull_requests[$j]->created_at . "\n";
+                }
             }
-            print_r(count($pull_requests));
-
-            // $mydate = date("Y-m-d", strtotime("-2 week"));
-            // print_r($mydate);
+            if (count($pull_requests) === 100) {
+                ++$n;
+            }
         }
     }
 }
--- Pull-Request-Watchdog/routes/api.php
+++ Pull-Request-Watchdog/routes/api.php
@@ -14,8 +14,4 @@ use App\Http\Controllers\WatchdogController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('getAllrequests',[WatchdogController::class,'getPullRequests']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('getAllrequests',[WatchdogController::class,'get14DaysPullRequests']);
